6147bb2e50bf3439c04fc086f58af91ec02c6f34
mike-axiom-mir <miketobi90@gmail.com>
2026-08-23 13:41:49 +0200

    Normalize district style state before renderer intake

---
+++ playable_3d/src/render/stateSafeParkIdentityWorldRenderer.js
@@ -0,0 +1,17 @@
+import { WorldRenderer as ParkIdentityWorldRenderer } from "./parkIdentityWorldRenderer.js";
+import { normalizeDistrictState } from "../core/districts.js";
+import { stateHash } from "../core/random.js";
+
+/**
+ * State-intake guard for the final presentation stack. New games do not need the
+ * preserved simulation to know about style-only district state, while loaded
+ * saves are already normalized by save.js. Re-hashing here prevents a renderer
+ * default from ever leaving stateHash stale.
+ */
+export class WorldRenderer extends ParkIdentityWorldRenderer {
+  setState(state) {
+    normalizeDistrictState(state);
+    state.stateHash = stateHash(state);
+    super.setState(state);
+  }
+}
