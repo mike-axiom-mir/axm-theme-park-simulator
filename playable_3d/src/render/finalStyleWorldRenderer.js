ff811a28b56d8b8534a5ac8886b79bab60dcaabf
mike-axiom-mir <miketobi90@gmail.com>
2026-08-23 23:14:47 +0200

    Route final style seam through seasonal and technology lands

--- playable_3d/src/render/finalStyleWorldRenderer.js
+++ playable_3d/src/render/finalStyleWorldRenderer.js
@@ -6,4 +6,4 @@
  * The state-integrity guard can therefore remain stable while the style stack
  * grows underneath it.
  */
-export { WorldRenderer } from "./medievalStyleWorldRenderer.js";
+export { WorldRenderer } from "./festivalTechStyleWorldRenderer.js";
