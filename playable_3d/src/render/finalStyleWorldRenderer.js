189f894038c538227b3adc0ee7c505d23d20677c
mike-axiom-mir <miketobi90@gmail.com>
2026-08-24 00:37:55 +0200

    themepark: route final style seam through Gilded Wealth

--- playable_3d/src/render/finalStyleWorldRenderer.js
+++ playable_3d/src/render/finalStyleWorldRenderer.js
@@ -6,4 +6,4 @@
  * The state-integrity guard can therefore remain stable while the style stack
  * grows underneath it.
  */
-export { WorldRenderer } from "./festivalTechMotionGuardWorldRenderer.js";
+export { WorldRenderer } from "./gildedStyleWorldRenderer.js";
