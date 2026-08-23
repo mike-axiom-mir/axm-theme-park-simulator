f3172f4053b57cecd8676030526e06e9503e431b
mike-axiom-mir <miketobi90@gmail.com>
2026-08-23 13:43:42 +0200

    Add touch-friendly district style control

--- playable_3d/src/render/stateSafeParkIdentityWorldRenderer.js
+++ playable_3d/src/render/stateSafeParkIdentityWorldRenderer.js
@@ -9,10 +9,33 @@ import { stateHash } from "../core/random.js";
  * default from ever leaving stateHash stale.
  */
 export class WorldRenderer extends ParkIdentityWorldRenderer {
+  constructor(canvas, callbacks = {}) {
+    super(canvas, callbacks);
+    const documentRef = globalThis.document;
+    this.districtStyleButton = documentRef?.getElementById?.("district-style-button") ?? null;
+    if (!this.districtStyleButton && documentRef?.createElement) {
+      const button = documentRef.createElement("button");
+      button.id = "district-style-button";
+      button.type = "button";
+      button.textContent = "District Style";
+      button.title = "Select a park element, then cycle the style of its district (Y)";
+      button.disabled = true;
+      button.addEventListener("click", () => this.cycleSelectedDistrictTheme());
+      documentRef.querySelector?.(".top-actions")?.prepend(button);
+      this.districtStyleButton = button;
+    }
+  }
+
   setState(state) {
     normalizeDistrictState(state);
     state.stateHash = stateHash(state);
     super.setState(state);
+    if (this.districtStyleButton) this.districtStyleButton.disabled = !this.selectedEntityId;
+  }
+
+  selectEntity(entityId) {
+    super.selectEntity(entityId);
+    if (this.districtStyleButton) this.districtStyleButton.disabled = !entityId;
   }
 
   cycleSelectedDistrictTheme() {
