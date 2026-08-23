a583a30b675a7237e1c544da05589ef79c2e8fec
mike-axiom-mir <miketobi90@gmail.com>
2026-08-23 23:06:40 +0200

    Add stable final style renderer seam

---
+++ playable_3d/src/render/finalStyleWorldRenderer.js
@@ -0,0 +1,9 @@
+/**
+ * Stable final-style seam.
+ *
+ * New presentation-only district styles should extend the current final style
+ * renderer and this file should be the only routing seam that needs to move.
+ * The state-integrity guard can therefore remain stable while the style stack
+ * grows underneath it.
+ */
+export { WorldRenderer } from "./medievalStyleWorldRenderer.js";
